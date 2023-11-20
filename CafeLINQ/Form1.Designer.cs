5bac58a95d79f4c0788414552c749c0a583c319a
heidipham12 <phamminhtram0978@gmail.com>
2023-11-20 11:53:31 +0700

    first commit

---
+++ CafeLINQ/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace CafeLINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dgwCaphe = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mFGDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.eXPDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.imageNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.capheBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.capheDBDataSet = new CafeLINQ.CapheDBDataSet();
+            this.picCaphe = new System.Windows.Forms.PictureBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnCloseApp = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblMFG = new System.Windows.Forms.Label();
+            this.lblEXP = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.capheTableAdapter = new CafeLINQ.CapheDBDataSetTableAdapters.CapheTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwCaphe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.capheBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.capheDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCaphe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgwCaphe
+            // 
+            this.dgwCaphe.AutoGenerateColumns = false;
+            this.dgwCaphe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwCaphe.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.typeDataGridViewTextBoxColumn,
+            this.mFGDataGridViewTextBoxColumn,
+            this.eXPDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn,
+            this.imageNameDataGridViewTextBoxColumn});
+            this.dgwCaphe.DataSource = this.capheBindingSource;
+            this.dgwCaphe.Location = new System.Drawing.Point(35, 12);
+            this.dgwCaphe.MultiSelect = false;
+            this.dgwCaphe.Name = "dgwCaphe";
+            this.dgwCaphe.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgwCaphe.Size = new System.Drawing.Size(527, 266);
+            this.dgwCaphe.TabIndex = 0;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // typeDataGridViewTextBoxColumn
+            // 
+            this.typeDataGridViewTextBoxColumn.DataPropertyName = "Type";
+            this.typeDataGridViewTextBoxColumn.HeaderText = "Type";
+            this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
+            // 
+            // mFGDataGridViewTextBoxColumn
+            // 
+            this.mFGDataGridViewTextBoxColumn.DataPropertyName = "MFG";
+            this.mFGDataGridViewTextBoxColumn.HeaderText = "MFG";
+            this.mFGDataGridViewTextBoxColumn.Name = "mFGDataGridViewTextBoxColumn";
+            // 
+            // eXPDataGridViewTextBoxColumn
+            // 
+            this.eXPDataGridViewTextBoxColumn.DataPropertyName = "EXP";
+            this.eXPDataGridViewTextBoxColumn.HeaderText = "EXP";
+            this.eXPDataGridViewTextBoxColumn.Name = "eXPDataGridViewTextBoxColumn";
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            // 
+            // imageNameDataGridViewTextBoxColumn
+            // 
+            this.imageNameDataGridViewTextBoxColumn.DataPropertyName = "ImageName";
+            this.imageNameDataGridViewTextBoxColumn.HeaderText = "ImageName";
+            this.imageNameDataGridViewTextBoxColumn.Name = "imageNameDataGridViewTextBoxColumn";
+            // 
+            // capheBindingSource
+            // 
+            this.capheBindingSource.DataMember = "Caphe";
+            this.capheBindingSource.DataSource = this.capheDBDataSet;
+            // 
+            // capheDBDataSet
+            // 
+            this.capheDBDataSet.DataSetName = "CapheDBDataSet";
+            this.capheDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // picCaphe
+            // 
+            this.picCaphe.Image = ((System.Drawing.Image)(resources.GetObject("picCaphe.Image")));
+            this.picCaphe.Location = new System.Drawing.Point(600, 69);
+            this.picCaphe.Name = "picCaphe";
+            this.picCaphe.Size = new System.Drawing.Size(240, 170);
+            this.picCaphe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picCaphe.TabIndex = 1;
+            this.picCaphe.TabStop = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Aqua;
+            this.btnDelete.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnDelete.Location = new System.Drawing.Point(553, 321);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 34);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Aqua;
+            this.btnUpdate.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(634, 321);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 34);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.Aqua;
+            this.btnInsert.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(715, 321);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 34);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnCloseApp
+            // 
+            this.btnCloseApp.BackColor = System.Drawing.Color.Aqua;
+            this.btnCloseApp.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCloseApp.Location = new System.Drawing.Point(796, 321);
+            this.btnCloseApp.Name = "btnCloseApp";
+            this.btnCloseApp.Size = new System.Drawing.Size(75, 34);
+            this.btnCloseApp.TabIndex = 5;
+            this.btnCloseApp.Text = "Close App";
+            this.btnCloseApp.UseVisualStyleBackColor = false;
+            this.btnCloseApp.Click += new System.EventHandler(this.btnCloseApp_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.capheBindingSource, "ID", true));
+            this.textBox1.Location = new System.Drawing.Point(90, 311);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.capheBindingSource, "MFG", true));
+            this.textBox2.Location = new System.Drawing.Point(327, 311);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.capheBindingSource, "EXP", true));
+            this.textBox3.Location = new System.Drawing.Point(327, 347);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(144, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.capheBindingSource, "Price", true));
+            this.textBox4.Location = new System.Drawing.Point(327, 385);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(144, 20);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.capheBindingSource, "Name", true));
+            this.textBox5.Location = new System.Drawing.Point(90, 347);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(127, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.capheBindingSource, "Type", true));
+            this.textBox6.Location = new System.Drawing.Point(90, 385);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(127, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(32, 314);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 12;
+            this.lblID.Text = "ID";
+            this.lblID.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(32, 354);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 13;
+            this.lblName.Text = "Name";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(32, 392);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(31, 13);
+            this.lblType.TabIndex = 14;
+            this.lblType.Text = "Type";
+            // 
+            // lblMFG
+            // 
+            this.lblMFG.AutoSize = true;
+            this.lblMFG.Location = new System.Drawing.Point(259, 314);
+            this.lblMFG.Name = "lblMFG";
+            this.lblMFG.Size = new System.Drawing.Size(30, 13);
+            this.lblMFG.TabIndex = 15;
+            this.lblMFG.Text = "MFG";
+            // 
+            // lblEXP
+            // 
+            this.lblEXP.AutoSize = true;
+            this.lblEXP.Location = new System.Drawing.Point(259, 354);
+            this.lblEXP.Name = "lblEXP";
+            this.lblEXP.Size = new System.Drawing.Size(28, 13);
+            this.lblEXP.TabIndex = 16;
+            this.lblEXP.Text = "EXP";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(259, 388);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(31, 13);
+            this.lblPrice.TabIndex = 17;
+            this.lblPrice.Text = "Price";
+            // 
+            // capheTableAdapter
+            // 
+            this.capheTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(871, 450);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblEXP);
+            this.Controls.Add(this.lblMFG);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCloseApp);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.picCaphe);
+            this.Controls.Add(this.dgwCaphe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwCaphe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.capheBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.capheDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCaphe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwCaphe;
+        private System.Windows.Forms.PictureBox picCaphe;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnCloseApp;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblMFG;
+        private System.Windows.Forms.Label lblEXP;
+        private System.Windows.Forms.Label lblPrice;
+        private CapheDBDataSet capheDBDataSet;
+        private System.Windows.Forms.BindingSource capheBindingSource;
+        private CapheDBDataSetTableAdapters.CapheTableAdapter capheTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn typeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mFGDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eXPDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn imageNameDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ CafeLINQ/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace CafeLINQ
+{
+    public partial class Form1 : Form
+    {
+        static string ProjectPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+        CapheDBDataContext db = new CapheDBDataContext();
+        public Form1()
+        {
+            InitializeComponent();
+            capheBindingSource.DataSource = db.Caphes;
+            capheBindingSource.EndEdit();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'capheDBDataSet.Caphe' table. You can move, or remove it, as needed.
+           this.capheTableAdapter.Fill(this.capheDBDataSet.Caphe);
+            picCaphe.Image = Image.FromFile(ProjectPath + "\\Data\\caphe.jpg");
+            picCaphe.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+        }
+        public void DisplayData()
+        {
+            //dgwCaphe.DataSource = (from c in db.Caphes select c);
+
+        }
+
+        private void btnCloseApp_Click(object sender, EventArgs e)
+        {
+            //Application.Exit();
+        }
+        private void dgwCaphe_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgwCaphe.Focused == false || dgwCaphe.CurrentCell == null)
+                return;
+            int RowIndex = dgwCaphe.CurrentRow.Index;
+            string SelectedID = dgwCaphe.Rows[RowIndex].Cells[0].Value.ToString();
+            string CapheImage = (from c in db.Caphes where c.ID == SelectedID select c.ImageName).FirstOrDefault();
+            if (CapheImage == null)
+                return;
+            picCaphe.Image = Image.FromFile(ProjectPath + "\\Data\\" + CapheImage);
+            picCaphe.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int RowIndex = dgwCaphe.CurrentRow.Index;
+            string SelectedID = dgwCaphe.Rows[RowIndex].Cells[0].Value.ToString();
+            Caphe deleteCP = (from c in db.Caphes where c.ID == SelectedID select c).SingleOrDefault();
+            db.Caphes.DeleteOnSubmit(deleteCP);
+            db.SubmitChanges();
+            DisplayData();
+            MessageBox.Show("Coffee: " + deleteCP.ID + "is deleted.");
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int RowIndex = dgwCaphe.CurrentRow.Index;
+            string SelectedID = dgwCaphe.Rows[RowIndex].Cells[0].Value.ToString();
+            Caphe updateedCP = (from c in db.Caphes where c.ID == SelectedID select c).SingleOrDefault();
+            updateedCP.ID = dgwCaphe.Rows[RowIndex].Cells[0].Value.ToString();
+            updateedCP.Name = dgwCaphe.Rows[RowIndex].Cells[1].Value.ToString();
+            updateedCP.Type = dgwCaphe.Rows[RowIndex].Cells[2].Value.ToString();
+            updateedCP.MFG = DateTime.Parse(dgwCaphe.Rows[RowIndex].Cells[3].Value.ToString());
+            updateedCP.EXP = DateTime.Parse(dgwCaphe.Rows[RowIndex].Cells[4].Value.ToString());
+            updateedCP.Price = Convert.ToInt32(dgwCaphe.Rows[RowIndex].Cells[5].Value.ToString());
+            updateedCP.ImageName = "caphe.jpg";
+
+            db.SubmitChanges();
+            DisplayData();
+            MessageBox.Show("Coffee: " + updateedCP.ID + "is updated.");
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            Caphe checkedCP;
+            List<Caphe> CPList = (from c in db.Caphes where c.ID.Contains("NEW") select c).ToList();
+            string NewID = "NEW";
+            if (CPList.Count == 0)
+                NewID = NewID + "001";
+            else
+            {
+                checkedCP = CPList.Last();
+                NewID = NewID + NextId(checkedCP.ID.Substring(checkedCP.ID.Length - 3));
+
+            }
+            Caphe insertedCP = new Caphe();
+            insertedCP.ID = NewID;
+            insertedCP.Name = "Not Assigned";
+            insertedCP.Type = "Not Assigned";
+            insertedCP.MFG = DateTime.Now;
+            insertedCP.EXP = DateTime.Now;
+            insertedCP.Price = 0;
+            insertedCP.ImageName = "caphe.jpg";
+
+            db.Caphes.InsertOnSubmit(insertedCP);
+            db.SubmitChanges();
+            DisplayData();
+            MessageBox.Show("New coffee is inserted! Please update data and press button Update ");
+        }
+       public string NextId(string currentId)
+        {
+            int i = 0;
+            if(int.TryParse(currentId, out i))
+            {
+                i++;
+                return (i.ToString().PadLeft(3, '0'));
+            }
+            throw new System.ArgumentException("Non-numeric string passed asargument");
+        }
+    }
+}
